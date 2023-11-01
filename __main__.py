fa1a73e0b567c4e35c5eb2111d5394a0251b0d96
SergiAguilo <saguilocastillo@gmail.com>
2023-11-01 17:35:14 +0100

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,80 @@
+# Steps
+# 1 - Take a random SH Github repo
+# 2 - Check if there is the database (when the database is created)
+# 3 - if database created, check md5. If not retrieve directly the README
+# 4 - Take the README and send it via API to the Model
+import os
+import requests
+import json
+from random import seed
+from random import randint
+
+# seed(1)
+
+def getMaxIdRepo():
+    newestGithubRepo = NewestRepo.split('https://github.com/')[1]
+    resp = requests.get(f"https://api.github.com/repos/{newestGithubRepo}", headers = header)
+
+    idNewestRepo = None
+    try:
+        if (resp.status_code == 200):
+            print(resp.json()['id'])
+            idNewestRepo = resp.json()['id']
+        else:
+            print(f"{newestGithubRepo} resulted in {resp}")
+    except:
+        print(f"Could not find ID in {newestGithubRepo}")
+    return idNewestRepo
+
+def randomRepo(maxRepoId):
+    repoId = randint(0, maxRepoId)
+    githubUrl = None
+    print(repoId)
+    # From the Repository Id, take the path from Github
+    reposUrl = f"https://api.github.com/repositories/{repoId}"
+    response = requests.get(reposUrl, headers = header)
+
+    if (response.status_code == 200):
+        githubUrl = response.json()['full_name']
+    else:
+        print(f"Github repo with Id {repoId} resulted in {response}")
+
+    return githubUrl
+
+def retrieveReadMe(randomGithubPath):
+    # Gather the README.md
+    readme_dict = None
+    try:
+        reposUrl = f"https://raw.githubusercontent.com/{randomGithubPath}/master/README.md"
+        response = requests.get(reposUrl, headers = header)
+        if (response.status_code == 200):
+            readme_dict = response.text
+        else:
+            print(f"Github with Path '{randomGithubPath}' resulted in {response}")
+    except:
+        print(f"Could not find READ.me in Path '{randomGithubPath}'")
+    return readme_dict
+
+def retrieveRandomReadMe():
+    idMaxRepo= getMaxIdRepo()
+    if not idMaxRepo:
+        return
+    randomGithubPath = randomRepo(idMaxRepo)    # Get Random repo
+    if not randomGithubPath:
+        return
+    readMe = retrieveReadMe(randomGithubPath)
+    if not readMe:
+        return
+    print(readMe)
+
+def confVariables():
+    githubToken = os.getenv('GITHUB_TOKEN', default='')
+    header = {'Authorization': 'Bearer ' + githubToken}
+    NewestRepo = os.getenv('NEWEST_REPO', default='https://github.com/SergiAguilo/SH-Worker')
+    return header, NewestRepo
+
+if __name__ == '__main__':
+    # Put your the url of your newest repository
+    header, NewestRepo = confVariables()
+    retrieveRandomReadMe()
+    
